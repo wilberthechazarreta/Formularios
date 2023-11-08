357f4667ecefdce506120cb29caa132a5b13d9e4
WilberthEC <wilberthechazarreta@gmail.com>
2023-11-08 02:47:22 -0500

    video 13

--- 08-formularios/src/app/app.module.ts
+++ 08-formularios/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 08-formularios/src/app/pages/template/template.component.ts
+++ 08-formularios/src/app/pages/template/template.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms'
 import { from } from 'rxjs';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -15,9 +16,14 @@ export class TemplateComponent implements OnInit {
     correo: 'wilberthechazarreta@gmail.com'
   }
 
-  constructor() {}
+  constructor( private paisService: PaisService ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+
+    this.paisService.getPaises()
+    .subscribe( paises => {
+      console.log(paises);
+    });
   }
 
   guardar( forma: NgForm ) {
