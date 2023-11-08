3c3cd69eae5a88b04826f04b156b786a13e4f53e
WilberthEC <wilberthechazarreta@gmail.com>
2023-11-07 23:41:39 -0500

    video 6

--- 08-formularios/README.md
+++ 08-formularios/README.md
@@ -1,4 +1,4 @@
-# 08Formularios
+# Formularios
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
 
---
+++ 08-formularios/src/app/pages/template/template.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  guardar(){
+    console.log('Submit disparado');
+  }
+
+}
